2c43118ff148b94ae090f281e54e194b7005f74c
liyanjun <liyanjun@qiyi.com>
2018-07-26 09:39:06 +0800

    添加nav

---
+++ iTest/HExtension/HNavigationController.h
@@ -0,0 +1,14 @@
+//
+//  HNavigationController.h
+//  reliao
+//
+//  Created by liyanjun on 2018/3/21.
+//  Copyright © 2018年 iqiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//使用自定义的push控件，防止push时卡顿
+@interface HNavigationController : UINavigationController
+
+@end
